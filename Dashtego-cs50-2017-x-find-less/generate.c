f0d73b461d61b4289a757fc08427e6eea49876d3
Ian Melville <melv009@gmail.com>
2017-03-24 01:38:59 -0400

    Add files via upload

---
+++ Dashtego-cs50-2017-x-find-less/generate.c
@@ -0,0 +1,52 @@
+/**
+ * generate.c
+ *
+ * Generates pseudorandom numbers in [0,MAX), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // If the number of arguments isn't two or three then print a statement explaining inputs
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // Assign n to be the first number inputed as an argument
+    int n = atoi(argv[1]);
+
+    // If a seed is used, then numbers will generate using the seed
+    if (argc == 3)
+    {
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
+    }
+
+    // Generates each number until specified # of numbers is reached.
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
